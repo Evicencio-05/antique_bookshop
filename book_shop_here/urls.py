2a29c5e11672d6d961024c4042b834ae3187f408
Evicencio-05 <evicencio5405@gmail.com>
2025-09-26 22:07:15 -0700

    Update URL's to match convections and new views.

--- book_shop_here/urls.py
+++ book_shop_here/urls.py
@@ -1,22 +1,21 @@
 from django.urls import path
 from . import views
 
+app_name = 'book_shop_here'
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    
-    path('books/', views.book_list, name='book_list'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/delete_book/<str:book_id>/', views.delete_book, name='delete_book'),
-    
-    path('groups/', views.group_list, name='group_list'),
-    path('groups/add/', views.add_group, name='add_group'),
-    
-    path('authors/', views.author_list, name='author_list'),
-    path('authors/add/', views.add_author, name='add_author'),
-    
-    path('orders/', views.order_list, name='order_list'),
-    path('orders/add/', views.add_order, name='add_order'),
-    
-    path('employees/', views.EmployeeListView.as_view(), name='employee_list'),
-    path('employees/add/', views.EmployeeCreateView.as_view(), name='add_employee'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('books/', views.BookListView.as_view(), name='book-list'),
+    path('books/add/', views.BookCreateView.as_view(), name='book-create'),
+    path('books/delete/<str:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('groups/', views.GroupListView.as_view(), name='group-list'),
+    path('groups/add/', views.GroupCreateView.as_view(), name='group-create'),
+    path('authors/', views.AuthorListView.as_view(), name='author-list'),
+    path('authors/add/', views.AuthorCreateView.as_view(), name='author-create'),
+    path('orders/', views.OrderListView.as_view(), name='order-list'),
+    path('orders/add/', views.OrderCreateView.as_view(), name='order-create'),
+    path('employees/', views.EmployeeListView.as_view(), name='employee-list'),
+    path('employees/add/', views.EmployeeCreateView.as_view(), name='employee-create'),
+    path('employees/edit/<int:pk>/', views.EmployeeUpdateView.as_view(), name='employee-update'),
+    path('employees/delete/<int:pk>/', views.EmployeeDeleteView.as_view(), name='employee-delete'),
 ]
