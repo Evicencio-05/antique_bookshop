774d241dac7112f8b1a0238a2b55227b8d7b1cd2
Evicencio-05 <evicencio5405@gmail.com>
2025-10-01 22:20:30 -0700

    Set up tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./book_shop_here/templates/**/*.html",
+    "./templates/**/*.html",
+    "./assets/**/*.{js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
