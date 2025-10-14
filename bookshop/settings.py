46b51c4bde01b06d15b93ca7553c0eaa6cf4d70d
Evicencio-05 <evicencio5405@gmail.com>
2025-10-13 22:32:11 -0700

    Added favicon.

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "book_shop_here/static"),
+]
 
 
 # Default primary key field type
